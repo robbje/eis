f62feb0202a70237d250b221fbd8db593ca5fb45
robbje <robbje@aachen.ccc.de>
2015-07-11 01:01:31 +0200

    implemented construction of equivalent circuit function from binary tree

--- eqc/eqc.py
+++ eqc/eqc.py
@@ -1,39 +1,53 @@
 #!/usr/bin/env python2
 
-class Eqc(object):
+from eqclib import get_eqc
+from parser import Node, Parser
+
+class EqcNode(Node):
     def __init__(self, *args, **kwargs):
+        Node.__init__(self)
         self.p = []
         self.eqc = lambda omega,p=self.p: 0
-        if 'tree' in kwargs:
-            self.eqc, self.p = self.fromTree(kwargs['tree'])
-        elif 'func' in kwargs:
-            self.p = kwargs['params'] if 'params' in kwargs else []
-            self.eqc = lambda omega,p=self.p: kwargs['func'](omega,p)
+        self.name = ""
 
-    def fromParseTree(self, root):
-        pass
-
-    def fromLibrary(self, identifier, p):
-        pass
+    def getCircuitFunc(self):
+        if self.value.type == 'SYMBOL':
+            ln, eqc, p = get_eqc(self.value.value)
+            self.p = p
+            self.name = ln
+            self.eqc = lambda w,p: eqc(w,p)
+            return self
+        elif self.value.type == 'PARALLEL':
+            a = self.left.getCircuitFunc()
+            a |= self.right.getCircuitFunc()
+            return a
+        elif self.value.type == 'SERIES':
+            a = self.left.getCircuitFunc()
+            a += self.right.getCircuitFunc()
+            return a
+        # This will result in an error, probably
+        return None
 
     def __add__(self,other):
         pu = len(self.p)
         self.p += other.p
-        p = self.p[pu:]
         f = self.eqc
-        self.eqc = lambda w,p=self.p: f(w,self.p) + other.eqc(w,self.p[pu:])
+        self.name = "(%s+%s)" % (self.name, other.name)
+        self.eqc = lambda w,p: f(w,p) + other.eqc(w,p[pu:])
         return self
 
     def __or__(self,other):
         pu = len(self.p)
         self.p += other.p
         f = self.eqc
-        self.eqc = lambda w,p=self.p: \
-            1.0/(1.0/f(w,self.p)+1.0/other.eqc(w,self.p[pu:]))
+        self.name = "(%s|%s)" % (self.name, other.name)
+        self.eqc = lambda w,p: \
+            1.0/(1.0/f(w,p)+1.0/other.eqc(w,p[pu:]))
         return self
 
 if __name__ == "__main__":
-    a = Eqc(func=lambda w,p: p[0], params=[1])
-    b = Eqc(func=lambda w,p: p[0], params=[4])
-    a |= b
-    print "R(1)||R(4)=R(%g)" % a.eqc(0)
+    p = Parser(EqcNode)
+    root = p.parse('R_4+(R_5|R_4)')
+    f = root.getCircuitFunc()
+    print f.name
+    print f.eqc(1, [2,1,4])
--- eqc/parser.py
+++ eqc/parser.py
@@ -2,11 +2,11 @@
 import ply.lex as lex
 
 class Node(object):
-    def __init__(self,value=None, parent=None):
+    def __init__(self):
         self.right = None
         self.left = None
-        self.value = value
-        self.parent = parent
+        self.value = None
+        self.parent = None
     def is_terminal(self):
         return self.value.type == 'SYMBOL'
     def __str__(self):
@@ -27,14 +27,15 @@ class Parser(object):
     t_PARALLEL = r'\|'
     t_LPAREN = r'\('
     t_RPAREN = r'\)'
-    t_SYMBOL = r'[a-zA-Z_]+'
+    t_SYMBOL = r'[a-zA-Z]+(_[a-zA-Z0-9]*|$^)*'
     def t_error(self, t):
         pass
-    def __init__(self):
+    def __init__(self, nodeobj):
         self.lexer = lex.lex(object=self)
+        self.nodeobj = nodeobj
     def parse(self, string):
         self.lexer.input(string)
-        self.root = Node()
+        self.root = self.nodeobj()
         self.current = self.root
         self.indent = 0
         while True:
@@ -50,7 +51,7 @@ class Parser(object):
             # parent node. If it doesn't exist, make a new root.
             self.current.value = t
             if not self.current.parent:
-                tmp = Node()
+                tmp = self.nodeobj()
                 self.current.parent = tmp
                 tmp.left = self.current
                 self.root = tmp
@@ -63,7 +64,8 @@ class Parser(object):
             self.indent += 1
             if self.current.left:
                 raise Exception("Unexpected parenthesis at pos %i" % t.lexpos)
-            self.current.left = Node(parent=self.current)
+            self.current.left = self.nodeobj()
+            self.current.left.parent = self.current
             self.current = self.current.left
         elif t.type == 'RPAREN':
             # Right parenthesis
@@ -73,6 +75,7 @@ class Parser(object):
             if self.indent < 0:
                 raise Exception("Unbalanced parenthesis at pos %i" % t.lexpos)
             if not self.current.right:
+                # TODO: There is a bug here, triggered by '()'
                 self.current.value = self.current.left.value
                 self.current.left = None
                 if not self.current.parent:
@@ -86,14 +89,16 @@ class Parser(object):
             if not self.current.left:
                 raise Exception("Syntax error at pos %i" % t.lexpos)
             if self.current.value:
-                tmp = Node(value=t)
+                tmp = self.nodeobj()
+                tmp.value = t
                 self.current.parent = tmp
                 tmp.left = self.current
                 self.root = tmp
                 self.current = tmp
             else:
                 self.current.value = t
-            self.current.right = Node(parent=self.current)
+            self.current.right = self.nodeobj()
+            self.current.right.parent = self.current
             self.current = self.current.right
 
 if __name__ == "__main__":
@@ -101,5 +106,5 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print "Usage: %s [string]"
         sys.exit(0)
-    p = Parser()
+    p = Parser(Node)
     print p.parse(sys.argv[1])
