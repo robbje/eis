Added jacobian test and fixed some jacobians

src/eqc/eqc.py
@@ -5,8 +5,17 @@ from copy import deepcopy
 import numpy as np
 from eqclib import getClassDefinition
 
+
 class CircuitTree(Node):
-    def __init__(self, params=[], eqc=lambda w,p: 0, name="", pNames = "", jac = []):
+
+    def __init__(
+            self,
+            params=[],
+            eqc=lambda w,
+            p: 0,
+            name="",
+            pNames="",
+            jac=[]):
         """Constructor for a CircuitTree node
 
             params: a list of numerical values
@@ -49,28 +58,30 @@ class CircuitTree(Node):
         self.jac = deepcopy(new.jac)
         return self
 
-    def __add__(self,other):
+    def __add__(self, other):
         pu = len(self.p)
         self.p = np.append(self.p, other.p)
         self.pNames += other.pNames
         f = self.eqc
         self.name = "(%s+%s)" % (self.name, other.name)
-        self.eqc = lambda w,p: f(w,p) + other.eqc(w,p[pu:])
-        self.jac += [lambda w,p: j(w,p[pu:]) for j in other.jac]
+        self.eqc = lambda w, p: f(w, p) + other.eqc(w, p[pu:])
+        self.jac += [lambda w, p: j(w, p[pu:]) for j in other.jac]
         return self
 
-    def __or__(self,other):
+    def __or__(self, other):
         pu = len(self.p)
         self.p = np.append(self.p, other.p)
         self.pNames += other.pNames
         f = self.eqc
         self.name = "(%s|%s)" % (self.name, other.name)
-        self.eqc = lambda w,p: \
-            1.0/(1.0/f(w,p)+1.0/other.eqc(w,p[pu:]))
-        for i,jac in enumerate(self.jac):
-            self.jac[i] = lambda w,p: np.power(other.eqc(w,p[pu:]),2.0)*jac(w,p)/ \
-                            np.power(other.eqc(w,p[pu:])+f(w,p),2.0)
+        self.eqc = lambda w, p: \
+            1.0 / (1.0 / f(w, p) + 1.0 / other.eqc(w, p[pu:]))
+        for i, jac in enumerate(self.jac):
+            self.jac[i] = lambda w, p: np.power(other.eqc(
+                w, p[pu:]), 2.0) * jac(w, p) / np.power(other.eqc(w, p[pu:]) + f(w, p), 2.0)
         for jac in other.jac:
-            self.jac.append(lambda w,p: np.power(f(w,p),2.0)*jac(w,p[pu:])/ \
-                            np.power(other.eqc(w,p[pu:])+f(w,p),2.0))
+            self.jac.append(lambda w, p: np.power(f(w, p), 2.0) *
+                            jac(w, p[pu:]) /
+                            np.power(other.eqc(w, p[pu:]) +
+                                     f(w, p), 2.0))
         return self

src/eqc/eqclib.py
@@ -3,81 +3,94 @@
 import numpy as np
 from copy import deepcopy
 
-def Warburg(w,p):
-    tan = 0
-    denom = np.power(1j*w*p[1],0.5)
-    if p[1]*w < 1e5:
-        tan = np.tanh(denom)
-    else:
-        tan = 1.0
-    return p[0] * tan/denom
 
-def dWarburg_dp1(w,p):
+def Warburg(w, p):
     tan = 0
-    denom = np.power(1j*w*p[1],0.5)
-    if p[1]*w < 1e5:
+    denom = np.power(1j * w * p[1], 0.5)
+    if p[1] * w < 1e5:
         tan = np.tanh(denom)
     else:
         tan = 1.0
-    return p[0]/(2*p[1]*np.power(np.cosh(np.power(1j*w*p[1],0.5)),2.0)) \
-            - 1j*p[0]*p[1]*tan/(2*np.power(1j*w*p[1],1.5))
+    return p[0] * tan / denom
+
+def dWarburg_dp0(w, p):
+    tan = 0
+    denom = np.power(1j * w * p[1], 0.5)
+    if p[1] * w < 1e5:
+        tan = np.tanh(denom)
+    else:
+        tan = 1.0
+    return 1*tan/denom
+
+def dWarburg_dp1(w, p):
+    tan = 0
+    denom = np.power(1j * w * p[1], 0.5)
+    if p[1] * w < 1e5:
+        tan = np.tanh(denom)
+    else:
+        tan = 1.0
+
+    real = p[0] / (2.0 * p[1] * np.power(np.cosh(np.power(1j * w * p[1], 0.5)),2.0))
+    imag = -1j * p[0] * w * tan / (2.0 * np.power(1j * w * p[1], 1.5))
+    return real+imag
 
 eqcLib = {
-    'R':{
-        'inuse':[],
-        'def':{
-            'eqc': lambda w,p: p[0],
+    'R': {
+        'inuse': [],
+        'def': {
+            'eqc': lambda w, p: p[0],
             'pNames': ['R'],
-            'jac': [lambda w,p: 1],
+            'jac': [lambda w, p: 1],
         },
     },
-    'C':{
-        'inuse':[],
-        'def':{
-            'eqc': lambda w,p: 1.0/(1j*w*p[0]),
+    'C': {
+        'inuse': [],
+        'def': {
+            'eqc': lambda w, p: 1.0 / (1j * w * p[0]),
             'pNames': ['C'],
-            'jac': [lambda w,p: -1.0/(1j*w*np.power(p[0],2.0))]
+            'jac': [lambda w, p: -1.0 / (1j * w * np.power(p[0], 2.0))]
         },
     },
-    'L':{
-        'inuse':[],
-        'def':{
-            'eqc': lambda w,p: 1j*w*p[0],
+    'L': {
+        'inuse': [],
+        'def': {
+            'eqc': lambda w, p: 1j * w * p[0],
             'pNames': ['L'],
-            'jac': [lambda w,p: 1j*w],
+            'jac': [lambda w, p: 1j * w],
         },
     },
-    'Zarc':{
-        'inuse':[],
-        'def':{
-            'eqc': lambda w,p: p[0]/(1+np.power(1j*p[1]*omega,p[2])),
-            'pNames': ['R','T','n'],
-            'jac': [lambda w,p: 1,
-                    lambda w,p: -1.0*p[0]*p[2]*np.power(1j*w*p[1],p[2])/ \
-                                p[1]*np.power(1.0+np.power(1j*w*p[1],p[2]),2.0),
-                    lambda w,p: -1.0*p[0]*np.power(1j*w*p[1],p[2])*np.log(1j*w*p[1])/ \
-                                np.power(1.0+np.power(1j*w*p[1],p[2]),2.0)],
+    'Zarc': {
+        'inuse': [],
+        'def': {
+            'eqc': lambda w, p: p[0] / (1.0 + np.power(1j * p[1] * w, p[2])),
+            'pNames': ['R', 'T', 'n'],
+            'jac': [lambda w, p: 1.0 / (1.0 + np.power(1j * p[1] * w, p[2])),
+                    lambda w, p: -1.0 * p[0] * p[2] * np.power(1j * w * p[1], p[2]) /
+                    (1.0 * p[1] * np.power(1.0 + np.power(1j * w * p[1], p[2]), 2.0)),
+                    lambda w, p: -1.0 * p[0] * np.power(1j * w * p[1], p[2]) * np.log(1j * w * p[1]) /
+                    np.power(1.0 + np.power(1j * w * p[1], p[2]), 2.0)],
         },
     },
-    'CPE':{
-        'inuse':[],
-        'def':{
-            'eqc': lambda w,p: 1.0/(p[0]*np.power(1j*w,p[1])),
-            'pNames': ['Q','n'],
-            'jac': [lambda w,p: -1.0/np.power(p[0],2.0),
-                    lambda w,p: np.log(1j*w)/(p[0]*np.power(1j*w,p[1]))],
+    'CPE': {
+        'inuse': [],
+        'def': {
+            'eqc': lambda w, p: 1.0 / (p[0] * np.power(1j * w, p[1])),
+            'pNames': ['Q', 'n'],
+            'jac': [lambda w, p: -1.0 * np.power(1j*w,-1.0*p[1])/np.power(p[0],2.0),
+                    lambda w, p: -1.0 * np.log(1j * w) / (p[0] * np.power(1j * w, p[1]))],
         },
     },
-    'Warb':{
-        'inuse':[],
-        'def':{
+    'Warb': {
+        'inuse': [],
+        'def': {
             'eqc': Warburg,
-            'pNames': ['R','T'],
-            'jac': [lambda w,p: 1,
+            'pNames': ['R', 'T'],
+            'jac': [dWarburg_dp0,
                     dWarburg_dp1],
         },
     },
 }
+
 
 def getClassDefinition(name):
     el = name
@@ -85,7 +98,7 @@ def getClassDefinition(name):
     if '_' in name:
         el = name.split('_')[0]
         ln = name.split('_')[1]
-    if not el in eqcLib:
+    if el not in eqcLib:
         raise ValueError("Unknown element: %s" % el)
         return None
     i = 1
@@ -95,7 +108,7 @@ def getClassDefinition(name):
     eqcLib[el]['inuse'] += [ln]
     classdef = deepcopy(eqcLib[el]['def'])
     classdef['name'] = '%s_%s' % (el, ln)
-    classdef['pNames'] = ['%s{%s}' % (classdef['name'], x) \
+    classdef['pNames'] = ['%s{%s}' % (classdef['name'], x)
                           for x in classdef['pNames']]
-    classdef['params'] = np.array([-1]*len(classdef['pNames']))
+    classdef['params'] = np.array([-1] * len(classdef['pNames']))
     return classdef